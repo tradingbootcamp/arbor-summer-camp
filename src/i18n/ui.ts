fd005472754acd10943a8ddcb35b95d9b17608bb
Ricki Heicklen <ricki.heicklen@gmail.com>
2025-03-21 12:08:50 -0400

    adding a bunch of homepage changes

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,6 +1,5 @@
 export const languages = {
-	en: "English",
-	it: "Italiano",
+	en: "English"
 };
 
 export const defaultLang = "en";
@@ -15,16 +14,16 @@ export const ui = {
 		services: "Services",
 		homepage: "Homepage",
 		tagline:
-			"Our aim is to make your website a key driver for sales and conversions, combining engaging design, easy navigation, and strong SEO. We focus on attracting visitors and leading them effortlessly to purchase, ensuring your products shine. Your success is our mission.",
-		"projects.yours": "Your project",
-		"projects.see": "See project",
-		"hero.title.main": "We create",
-		"hero.title.1": "websites",
-		"hero.title.2": "apps",
-		"hero.title.3": "designs",
-		"hero.title.4": "brands",
+			"A container for the greater Lighthaven community to run intimate, multi-day events in the most target-rich environment that exists for them, anywhere in the world, any time in the year.",
+		"projects.yours": "Your branch",
+		"projects.see": "See branch",
+		"hero.title.main": "Arbor Summer Camp",
+		"hero.title.1": "trading",
+		"hero.title.2": "Rationality",
+		"hero.title.3": "Writing",
+		"hero.title.4": "finger fencing", // ricki remember to replace this! 
 		"hero.subtitle":
-			"Our digital agency specializes in crafting captivating websites that convert and sell. Contact us for a free consultation.",
+			"A container for the greater Lighthaven community to run intimate, multi-day events in the most target-rich environment that exists for them, anywhere in the world, any time in the year.",
 		"hero.scroll": "scroll down to explore more",
 		"websites.title": "Websites",
 		"apps.title": "Apps",
@@ -86,91 +85,7 @@ export const ui = {
 		"footer.newsletter": "Subscribe to",
 		"footer.newsletter2": "our newsletter",
 		"footer.yourmail": "Your email",
-	},
-	it: {
-		or: "o",
-		contact: "Contattaci",
-		contacts: "Contatti",
-		contactus: "Contattaci",
-		projects: "Progetti",
-		services: "Servizi",
-		homepage: "Homepage",
-		tagline:
-			"In nostro obbiettivo è trasformare il tuo sito web in un catalizzatore potente di vendite e conversioni, fondendo un design accattivante con una navigazione intuitiva e un SEO efficace. Puntiamo a catturare l'attenzione dei visitatori e guidarli fluidamente verso l'acquisto, assicurandoci che i tuoi prodotti spicchino. Il tuo successo è il fulcro della nostra missione.",
-		"projects.yours": "Il tuo progetto",
-		"projects.see": "Visualizza progetto",
-		"hero.title.main": "Creiamo",
-		"hero.title.1": "siti",
-		"hero.title.2": "app",
-		"hero.title.3": "design",
-		"hero.title.4": "brand",
-		"hero.subtitle":
-			"Trasforma la tua visione in realtà con la nostra agenzia digitale. Specializzati in siti web innovativi che catturano l'attenzione e massimizzano le vendite, siamo pronti a elevare il tuo brand. Approfitta della nostra consulenza gratuita per iniziare.",
-		"hero.scroll": "scorri per scoprire di più",
-		"websites.title": "Siti",
-		"apps.title": "App",
-		"uiux.title": "Design UI/UX",
-		"seo.title": "SEO",
-		"advertising.title": "Pubblicità",
-		"websites.content": "Da Mintaka Studio, siamo specializzati nella realizzazione di siti web su misura per le esigenze del tuo marchio. Il nostro approccio combina design estetico con competenza tecnica, garantendo che il tuo sito sia visivamente accattivante, veloce e reattivo su tutti i dispositivi.",
-		"apps.content": "Offriamo servizi di sviluppo app di alto livello, concentrati sulla creazione di applicazioni mobili intuitive, efficienti e coinvolgenti. Sia per iOS, Android o piattaforme incrociate, le nostre app sono progettate per migliorare l'esperienza utente e favorire la crescita aziendale.",
-		"uiux.content": "I nostri servizi di design UI/UX sono incentrati sulla creazione di interfacce centrate sull'utente che sono sia visivamente attraenti che facili da navigare. Diamo priorità all'esperienza utente, assicurando che ogni elemento di design sia in linea con le esigenze degli utenti e migliori l'interazione complessiva con i tuoi prodotti digitali.",
-		"seo.content": "I servizi SEO di Mintaka Studio sono progettati per aumentare la visibilità del tuo sito web e generare traffico organico. Utilizziamo una combinazione delle più recenti strategie e tecniche SEO per migliorare il tuo posizionamento sui motori di ricerca, concentrando sia sull'ottimizzazione on-page che off-page.",
-		"advertising.content": `I nostri servizi pubblicitari mirano a massimizzare il tuo ROI attraverso campagne pubblicitarie mirate ed efficaci. Siamo specializzati nella creazione di contenuti pubblicitari avvincenti e strategie che risuonano con il tuo pubblico, utilizzando varie piattaforme come Google Ads e i social media per potenziare la portata e l'engagement del tuo marchio.`,
-		"privacy.wip": "Lavori in corso",
-		"privacy.wip.content": "Questa pagina verrà aggiornata a breve",
-		"faqs.question1":
-			"Qual è il vostro linguaggio di programmazione preferito?",
-		"faqs.answer1":
-			"Amiamo TypeScript per la sua sicurezza dei tipi e le sue caratteristiche moderne, ma siamo anche esperti in Python, JavaScript e Go.",
-		"faqs.question2": "Avete una macchina del caffè in ufficio?",
-		"faqs.answer2":
-			"Sì, ed è probabilmente l'attrezzatura più importante che abbiamo! Funzioniamo a base di caffeina e creatività.",
-		"faqs.question3":
-			"Qual è il tuo approccio alla risoluzione dei problemi con il cane di gomma?",
-		"faqs.answer3":
-			"Abbiamo una collezione di cani di gomma, ciascuno specializzato in linguaggi di programmazione diversi. Sono efficaci sorprendentemente nel trovare i bug!",
-		"faqs.question4": "Come gestisci i conflitti di fusione?",
-		"faqs.answer4":
-			"Con pazienza, caffè e a volte un po' di panico. Ma per lo più con buone pratiche Git e chiara comunicazione tra i membri del team.",
-		"faqs.question5": "Qual è la tua posizione su tabs vs spazi?",
-		"faqs.answer5":
-			"Questa è una domanda trappola! Usiamo EditorConfig per mantenere la consistenza nel team. Ma in segreto, siamo team spazi.",
-		"faqs.question6": "Quanti monitor usano i tuoi sviluppatori?",
-		"faqs.answer6":
-			"La risposta corretta è n+1, dove n è il numero corrente di monitor. Ci sarà sempre spazio per uno schermo in più!",
-		"faqs.question7": "Qual è la tua strategia di debug?",
-		"faqs.answer7":
-			"Prima proviamo console.log, poi proviamo strumenti di debug appropriati, infine ci fissiamo sul codice fino a quando non confessa i suoi bug.",
-		"faqs.question8": "Come festeggiamo i deployment riusciti?",
-		"faqs.answer8":
-			"Con una miscela di sollievo, pugni, e occasionalmente pizza. Poi subito iniziamo a preoccuparci del prossimo deployment.",
-		"faqs.question9": "Qual è il tema IDE preferito?",
-		"faqs.answer9":
-			"Tema scuro durante il giorno, tema più scuro di notte. I nostri sviluppatori sono come vampiri - evitiamo luci forti.",
-		"faqs.question10": "Come chiami le tue variabili?",
-		"faqs.answer10":
-			"Seguiamo convenzioni di denominazione rigorose, ma a volte ci ritroviamo con classici come 'finalFinalVersionForReal' e 'thisWillDefinitelyWork'.",
-		"faq.otherquestions": "Altre domande?",
-		"thanks.subtitle": "Mail inviata con successo",
-		"thanks.title": "Grazie",
-		"thanks.content": "Ti risponderemo il prima possibile",
-		"contact.title": "Contatti",
-		"contact.subtitle":
-			"I piccioni viaggiatori sono i benvenuti, ma la posta elettronica è più veloce!",
-		"contact.name": "Nome",
-		"contact.email": "Email",
-		"contact.message": "Messaggio",
-		"contact.company": "Azienda",
-		"contact.agree": "Cliccando invia accetti la nostra",
-		"contact.send": "Invia",
-		"blog.title": "Blog",
-		"blog.subtitle": "Tecnologia, consigli e refusi!",
-		"blog.gotoproject": "Vai al progetto",
-		"footer.newsletter": "Iscriviti alla",
-		"footer.newsletter2": "newsletter",
-		"footer.yourmail": "La tua email",
-	},
+	}
 } as const;
 
 export const showDefaultLang = false;
