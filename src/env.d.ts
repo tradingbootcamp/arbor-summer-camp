c7140ee52fde530d5995838d474c03a57b54a18c
Ricki Heicklen <ricki.heicklen@gmail.com>
2025-03-21 18:34:00 -0400

    resizing

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elemName: string]: any;
+  }
+}
