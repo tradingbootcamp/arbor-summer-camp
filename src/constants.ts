e6f7346ed6a2806e50eacd36f1fd0a8b2870ce04
Ricki Heicklen <ricki.heicklen@gmail.com>
2025-05-21 12:04:08 -0400

    making airtable links into constants so that I stop fucking up what is where

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+// External services URLs
+export const URLS = {
+  // Ticket/registration links
+  HAVEN_BOOKINGS: "https://www.havenbookings.space/festival-season",
+  HAVEN_BOOKINGS_BRANCHES: "https://www.havenbookings.space/festival-season#branches",
+  SC_TICKETS: "https://bit.ly/sc-tickets",
+  
+  // Form links
+  AIRTABLE_BRANCH_SIGNUP: "https://airtable.com/apprhYrZf0T8NcmEp/pagqeE8CJT0WopSRp/form",
+  AIRTABLE_BRANCH_RFP: "https://airtable.com/appTX8ljKL2tArGsn/pagVYRe2JGYyIWm11/form"
+}; 
